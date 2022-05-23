03a1a3f0a163bd3934ace903013e40317ec63487
Sergey Bobrovich <siarhey1987@gmail.com>
2022-05-23 22:02:28 +0300

    Changed pom, web.xml, ddl.sql

--- JSON/ddl.sql
+++ JSON/ddl.sql
@@ -6,40 +6,21 @@ CREATE SCHEMA IF NOT EXISTS courses
 COMMENT ON SCHEMA courses
     IS 'Groups and students dataBase';
 
--- Table: courses.groupStudentsList
 CREATE TABLE courses.Groups (
-    id BIGSERIAL NOT NULL,
-    name VARCHAR PRIMARY KEY NOT NULL
-);
+id BIGSERIAL  NOT NULL,
+name VARCHAR(255) NOT NULL UNIQUE,
+PRIMARY KEY (id,name));
 
 CREATE TABLE courses.Students (
-    id BIGSERIAL PRIMARY KEY  NOT NULL,
-    name VARCHAR NOT NULL,
-    age SMALLINT NOT NULL,
-    score DOUBLE PRECISION,
-    olimpic_gamer BOOLEAN
-);
+id BIGSERIAL PRIMARY KEY  NOT NULL,
+name VARCHAR(255) NOT NULL,
+age INT NOT NULL,
+score DOUBLE PRECISION,
+olympic_gamer BOOLEAN);
 
-CREATE TABLE courses.Students_in_groupe (
-    groupe_name VARCHAR NOT NULL,
-    student_id BIGINT NOT NULL UNIQUE
-);
+CREATE TABLE courses.Students_in_groups (
+group_name VARCHAR(255) NOT NULL,
+student_id BIGINT NOT NULL UNIQUE);
 
-ALTER TABLE
-    courses.Students_in_groupe
-    ADD CONSTRAINT
-        Students_in_groupe_groupe_name_Groups_name
-    FOREIGN KEY
-        (groupe_name)
-    REFERENCES
-        courses.Groups(name) ON DELETE CASCADE ON UPDATE CASCADE;
-
-ALTER TABLE
-    courses.Students_in_groupe
-    ADD CONSTRAINT
-        Students_in_groupe_student_id_Students_id
-    FOREIGN KEY
-        (student_id)
-    REFERENCES
-        courses.Students(id)
-        ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE courses.Students_in_groups ADD CONSTRAINT Students_in_groups_group_name_Groups_name FOREIGN KEY (group_name) REFERENCES courses.Groups(name) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE courses.Students_in_groups ADD CONSTRAINT Students_in_groups_student_id_Students_id FOREIGN KEY (student_id) REFERENCES courses.Students(id) ON DELETE CASCADE ON UPDATE CASCADE;
