6fffd91c903a6dc229843915fc93b5ff46fc6f0d
Sergey Bobrovich <siarhey1987@gmail.com>
2022-05-23 23:48:14 +0300

    Formatted ddl.sql

--- JSON/ddl.sql
+++ JSON/ddl.sql
@@ -1,26 +1,35 @@
--- Schema
+CREATE SCHEMA IF NOT EXISTS courses AUTHORIZATION postgres;
 
-CREATE SCHEMA IF NOT EXISTS courses
-    AUTHORIZATION postgres;
+COMMENT ON SCHEMA courses IS 'Groups and students dataBase';
 
-COMMENT ON SCHEMA courses
-    IS 'Groups and students dataBase';
+CREATE TABLE courses.groups
+  (
+     id   BIGSERIAL NOT NULL,
+     name VARCHAR(255) NOT NULL UNIQUE,
+     PRIMARY KEY (id, name)
+  );
 
-CREATE TABLE courses.Groups (
-id BIGSERIAL  NOT NULL,
-name VARCHAR(255) NOT NULL UNIQUE,
-PRIMARY KEY (id,name));
+CREATE TABLE courses.students
+  (
+     id            BIGSERIAL PRIMARY KEY NOT NULL,
+     name          VARCHAR(255) NOT NULL,
+     age           INT NOT NULL,
+     score         DOUBLE PRECISION,
+     olympic_gamer BOOLEAN
+  );
 
-CREATE TABLE courses.Students (
-id BIGSERIAL PRIMARY KEY  NOT NULL,
-name VARCHAR(255) NOT NULL,
-age INT NOT NULL,
-score DOUBLE PRECISION,
-olympic_gamer BOOLEAN);
+CREATE TABLE courses.students_in_groups
+  (
+     group_name VARCHAR(255) NOT NULL,
+     student_id BIGINT NOT NULL UNIQUE
+  );
 
-CREATE TABLE courses.Students_in_groups (
-group_name VARCHAR(255) NOT NULL,
-student_id BIGINT NOT NULL UNIQUE);
+ALTER TABLE courses.students_in_groups
+  ADD CONSTRAINT students_in_groups_group_name_groups_name FOREIGN KEY (
+  group_name) REFERENCES courses.groups(name) ON DELETE cascade ON UPDATE
+  cascade;
 
-ALTER TABLE courses.Students_in_groups ADD CONSTRAINT Students_in_groups_group_name_Groups_name FOREIGN KEY (group_name) REFERENCES courses.Groups(name) ON DELETE CASCADE ON UPDATE CASCADE;
-ALTER TABLE courses.Students_in_groups ADD CONSTRAINT Students_in_groups_student_id_Students_id FOREIGN KEY (student_id) REFERENCES courses.Students(id) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE courses.students_in_groups
+  ADD CONSTRAINT students_in_groups_student_id_students_id FOREIGN KEY (
+  student_id) REFERENCES courses.students(id) ON DELETE cascade ON UPDATE
+  cascade; 
